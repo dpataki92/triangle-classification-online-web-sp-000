7c0877425dd2eccf9b5ec70c96dc4836263ffef7
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-02 21:40:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,7 +27,7 @@ class Triangle
      true_or_false = true
      if side1 <= 0 || side2 <= 0 || side3 <= 0
        true_or_false = false
-     elsif side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1
+     elsif side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
        true_or_false = false
      end
       true_or_false
